50719e79093779c49e79c55fd133743f3d969287
canonius <canonius@gmail.com>
2023-03-14 00:29:04 +0900

    add: puppy model

--- puppies/models.py
+++ puppies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Puppy(models.Model):
+    """
+    Poppy Model
+    Defines the attributes of a puppy
+    """
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_breed(self):
+        return self.name + ' belongs to ' + self.breed + ' breed.'
+    def __repr__(self) -> str:
+        return self.name + ' is added.'
+    
